bdb57bcf21fc9d10d4319568e96e93bebe6819ac
Aditya Oberai <424@doonschool.com>
2020-08-13 21:12:58 +0530

    Added Xamarin.Forms app
    * Has "Click A Picture" functionality
    * Need to add "Select A Picture"

---
+++ GarudaHacks/GarudaHacks/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GarudaHacks
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new NavigationPage(new GarudaHacks.MainPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ GarudaHacks/GarudaHacks/ImageCapture.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Diagnostics;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
+using Plugin.Media;
+
+namespace GarudaHacks
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ImageCapture : ContentPage
+    {
+        public ImageCapture()
+        {
+            InitializeComponent();
+        }
+        private async void clickImage_Clicked(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                Debug.WriteLine("No Camera Available");
+                return;
+            }
+
+            var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+            {
+                Directory = "Sample",
+                Name = "test.jpg",
+                AllowCropping = false
+            });
+
+            if (file == null)
+                return;
+
+            await DisplayAlert("File Location", file.Path, "OK");
+
+            image.Source = ImageSource.FromStream(() =>
+            {
+                var stream = file.GetStream();
+                return stream;
+            });
+            Debug.WriteLine(image.Source);
+        }
+
+        private void sendToCodeSpace_Clicked(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Send To CodeSpace Button Works!!!");
+        }
+    }
+}
